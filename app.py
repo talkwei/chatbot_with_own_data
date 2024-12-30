4a340156026a2f8f410b39400a7c0d8e18899f0e
Vivant Shen <vivant@lendingworks.co.uk>
2024-12-30 14:23:33 +0000

    amended to the latest version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 import streamlit as st
 # from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.core import VectorStoreIndex,ServiceContext
+from llama_index.core import VectorStoreIndex,Settings
 from llama_index.llms.openai import OpenAI
 import openai
 from llama_index.core import SimpleDirectoryReader
@@ -23,9 +23,8 @@ def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5,
-                                                                  system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5,system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
+        index = VectorStoreIndex.from_documents(docs)
         return index
 
 
@@ -52,3 +51,10 @@ if st.session_state.messages[-1]["role"] != "assistant":
 
 
 
+#  ServiceContext is deprecated. Use llama_index.settings.Settings
+#
+# instead, or pass in modules to local functions/methods/interfaces.
+#
+# See the docs for updated usage/migration:
+#
+# https://docs.llamaindex.ai/en/stable/module_guides/supporting_modules/service_context_migration/
